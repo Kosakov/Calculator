028df52926cfe557a5deba9795a757c9458a1bf0
Kosakov <nikolaikosakov@gmail.com>
2024-04-01 21:03:30 +0200

    Fixed floating point bug

--- script.js
+++ script.js
@@ -1,98 +1,112 @@
-let buttons=document.querySelectorAll(".button");
-let display=document.querySelector("#displaySum");
-let firstNum=""
-let secondNum=""
-let operator=""
-let hasResult=false
+let buttons = document.querySelectorAll(".button");
+let display = document.querySelector("#displaySum");
+let floatingPointBtn=document.querySelector(".floatingPoint");
+let firstNum = ""
+let secondNum = ""
+let operator = ""
+let hasResult = false
+let floatingCounts = 0
 
 console.log(buttons)
 
-buttons.forEach(button=>button.addEventListener("click",log))
+if (checkfloatingPointsCount(display.textContent) >0){
+    console.log(checkfloatingPointsCount(display.textContent))
+    floatingPointBtn.removeEventListener("click", log)
+}
 
-function log(e){
-    let target=e.target
-    let button=target.textContent
+buttons.forEach(button => button.addEventListener("click", log))
+
+function log(e) {
+    let target = e.target
+    let button = target.textContent
 
     checkContentLength()
 
-    //console.log(button)
-    if(button=="AC"){
-        display.textContent=""
-        hasResult=false
-    }
-    
-
-    
-    if (button==0 || button==1 || button==2 || button==3 || button==4 || button==5 || button==6 || button==7 || button==8 || button==9 || button==".")
-    {
-        if(display.textContent.length>10 || hasResult || checkDecimalPointsCount(display.textContent)){
-            return
-        }
-        //if(hasResult){
-        //    return
-        //}
-        display.textContent=display.textContent+button
+    if (checkfloatingPointsCount(display.textContent) >0){
+        console.log(checkfloatingPointsCount(display.textContent))
+        floatingPointBtn.removeEventListener("click", log)
     }
 
-    if (button=="+" || button=="-" || button=="*" || button=="/" || button=="%"){
-        operator=button
-        firstNum=display.textContent
-        hasResult=false
-        display.textContent=""
+    console.log(button)
+    if (button == "AC") {
+        display.textContent = ""
+        hasResult = false
+        firstNum = ""
+        secondNum = ""
+        operator = ""
     }
 
-    if (button=="="){
-        secondNum=display.textContent
-        console.log(firstNum)
-        console.log(secondNum)
-        let result=calculate(firstNum,secondNum,operator)
+    if (display.textContent.length > 10 || hasResult) {
+        return
+    }
+
+    if (button == 0 || button == 1 || button == 2 || button == 3 || button == 4 || button == 5 || button == 6 || button == 7 || button == 8 || button == 9 || button == ".") {
+
+        display.textContent = display.textContent + button
+    }
+
+    if (button == "+" || button == "-" || button == "*" || button == "/" || button == "%") {
+        operator = button
+        firstNum = display.textContent
+        hasResult = false
+        display.textContent = ""
+        floatingPointBtn.addEventListener("click", log)
+    }
+
+    if (button == "=") {
+        let result = calculate(firstNum, secondNum, operator)
+        secondNum = display.textContent
         checkContentLength(result)
-        display.textContent=calculate(firstNum,secondNum,operator)
-        hasResult=true
+        display.textContent = calculate(firstNum, secondNum, operator)
+        hasResult = true
     }
 
 }
 
-function calculate(firstNumber,secondNumber,operator){
-    if (operator==="+"){
-        return Number(firstNumber)+Number(secondNumber)
+function calculate(firstNumber, secondNumber, operator) {
+    if (operator === "+") {
+        return (Number(firstNumber) + Number(secondNumber)).toFixed(2)
     }
-    if (operator==="-"){
-        return Number(firstNumber)-Number(secondNumber)
+    if (operator === "-") {
+        return (Number(firstNumber) - Number(secondNumber)).toFixed(2)
     }
-    if (operator==="*"){
-        return Number(firstNumber)*Number(secondNumber)
+    if (operator === "*") {
+        return (Number(firstNumber) * Number(secondNumber)).toFixed(2)
     }
-    if (operator==="/"){
-        if (firstNumber==0 || secondNumber==0){
+    if (operator === "/") {
+        if (firstNumber == 0 || secondNumber == 0) {
             return "Invalid"
         }
-        return Number(firstNumber)/Number(secondNumber)
+        return (Number(firstNumber) / Number(secondNumber)).toFixed(2)
     }
-    if (operator==="%"){
-        return Number(firstNumber)%Number(secondNumber)
+    if (operator === "%") {
+        return (Number(firstNumber) % Number(secondNumber)).toFixed(2)
+    }
+}
+
+
+
+function checkContentLength(content) {
+
+    if (String(content).length > 10 && String(content).length < 20) {
+        display.style.fontSize = "1.5em"
+    }
+    else if (String(content).lengt >= 20) {
+        display.style.fontSize = "1.5em"
+    }
+    else {
+        display.style.fontSize = "2em"
     }
 }
 
 
+function checkfloatingPointsCount(content) {
+    let matches = content.match(/\./g);
 
-function checkContentLength(content){
-    
-    if (String(content).length>10 && String(content).length<20){
-        display.style.fontSize="1.5em"
+    if (!matches) {
+        return 0;
     }
-    else if (String(content).lengt>=20){
-        display.style.fontSize="1.5em"
-    }
-    else{
-        display.style.fontSize="2em"
-    }
+    return matches.length;
 }
 
-//function checkDecimalPointsCount(content){
-//    let regex="/^[^.]*\.[^.]*[^.]$/g"
-//    if (((content.split(".").length)>2 && content.split(".")[1]!=="")){
-//    console.log(content.split("."))
-//    return true
-//}
-//}
+
