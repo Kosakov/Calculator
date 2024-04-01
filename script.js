1ffd088674dea07e078690b158202c66dc4cc8d3
Kosakov <nikolaikosakov@gmail.com>
2024-04-01 22:01:22 +0200

    ADD comments and fix some minor bugs

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// Selecting all elements with class "button"
 let buttons = document.querySelectorAll(".button");
 let display = document.querySelector("#displaySum");
+
 let floatingPointBtn=document.querySelector(".floatingPoint");
 let firstNum = ""
 let secondNum = ""
 let operator = ""
 let hasResult = false
+let displayContent=display.textContent
 
+// Add eventListeners to all buttons
+buttons.forEach(button => button.addEventListener("click", handleButtonClick))
 
-buttons.forEach(button => button.addEventListener("click", log))
-
-function log(e) {
+function handleButtonClick(e) {
     let target = e.target
     let button = target.textContent
 
     checkContentLength()
-
+    
+    // Checking if the display contains more than one floating point
     if (checkfloatingPointsCount(display.textContent) >0){
         console.log(checkfloatingPointsCount(display.textContent))
-        floatingPointBtn.removeEventListener("click", log)
+        floatingPointBtn.removeEventListener("click", handleButtonClick)
     }
 
     //console.log(button)
@@ -29,62 +33,76 @@ function log(e) {
         operator = ""
     }
 
-    if (button == 0 || button == 2 || button == 2 || button == 3 || button == 4 || button == 5 || button == 6 || button == 7 || button == 8 || button == 9 || button == ".") {
+    // Handling digit buttons and floating point: appending to display if length is within limit and no result is present
+    if (button == 0 || button == 1 || button == 2 || button == 3 || button == 4 || button == 5 || button == 6 || button == 7 || button == 8 || button == 9 || button == ".") {
         if (display.textContent.length > 10 || hasResult) {
             return
         }
         display.textContent = display.textContent + button
     }
-
+    // Handling operator buttons: setting operator, storing first number, clearing display, and adding event listener to floating point button
     if (button == "+" || button == "-" || button == "*" || button == "/" || button == "%") {
         operator = button
         firstNum = display.textContent
         hasResult = false
         display.textContent = ""
-        floatingPointBtn.addEventListener("click", log)
+        floatingPointBtn.addEventListener("click", handleButtonClick)
     }
 
+    // Handling equals button: storing second number, calculating result, updating display, setting result status, and adding event listener to floating point button
     if (button == "=") {
         secondNum = display.textContent
         let result = calculate(firstNum, secondNum, operator)
         checkContentLength(result)
-        console.log(result)
-        if(result){
-            display.textContent=result
-        }
-        else{
-            display.textContent = "Inalid Operation"
-        }
-        
+        display.textContent=result
         hasResult = true
-        floatingPointBtn.addEventListener("click", log)
+        floatingPointBtn.addEventListener("click", handleButtonClick)
     }
 
 }
+// Function to perform the calculation
 
 function calculate(firstNumber, secondNumber, operator) {
+    if(firstNumber && secondNumber && operator){
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+    let result
+
     if (operator === "+") {
-        return (Number(firstNumber) + Number(secondNumber)).toFixed(2)
+        result= firstNumber+secondNumber
     }
     if (operator === "-") {
-        return (Number(firstNumber) - Number(secondNumber)).toFixed(2)
+        result= firstNumber-secondNumber
     }
     if (operator === "*") {
-        return (Number(firstNumber) * Number(secondNumber)).toFixed(2)
+        result= firstNumber*secondNumber
     }
     if (operator === "/") {
+        // It's impossible to divide by 0
         if (firstNumber == 0 || secondNumber == 0) {
             return "Invalid"
         }
-        return (Number(firstNumber) / Number(secondNumber)).toFixed(2)
+        result= firstNumber/secondNumber
     }
+
     if (operator === "%") {
-        return (Number(firstNumber) % Number(secondNumber)).toFixed(2)
+        result= firstNumber%secondNumber
+    }
+// Checks if the result id floating point number-->If true, the result has only 2 digits after the floating point
+    if (result%1!==0){
+        result=result.toFixed(2)
+    }
+    return result
+}
+    else{
+        return
     }
 }
 
 
 
+
+// Function to adjust font size based on content length
 function checkContentLength(content) {
 
     if (String(content).length > 10 && String(content).length < 20) {
@@ -98,6 +116,7 @@ function checkContentLength(content) {
     }
 }
 
+// Function to check the count of floating points in content
 
 function checkfloatingPointsCount(content) {
     let matches = content.match(/\./g);
