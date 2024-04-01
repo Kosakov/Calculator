a87bfead2afdf23d204cf6e870b12031b894cca7
Kosakov <nikolaikosakov@gmail.com>
2024-04-01 18:12:52 +0200

    ADD functionality Added 2 new buttons:double zero and decimal point and make the "%" button to work

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let display=document.querySelector("#displaySum");
 let firstNum=""
 let secondNum=""
 let operator=""
+let hasResult=false
 
 console.log(buttons)
 
@@ -12,22 +13,31 @@ function log(e){
     let target=e.target
     let button=target.textContent
 
-    console.log(button)
+    checkContentLength()
 
+    //console.log(button)
     if(button=="AC"){
         display.textContent=""
+        hasResult=false
     }
-
-    //(button==0 || button==1 || button==2 || button==3 || button==4 || button==5 || button==6 || button==7 || button==8 || button==9)
     
-    if (button==0 || button==1 || button==2 || button==3 || button==4 || button==5 || button==6 || button==7 || button==8 || button==9)
+
+    
+    if (button==0 || button==1 || button==2 || button==3 || button==4 || button==5 || button==6 || button==7 || button==8 || button==9 || button==".")
     {
+        if(display.textContent.length>10 || hasResult || checkDecimalPointsCount(display.textContent)){
+            return
+        }
+        //if(hasResult){
+        //    return
+        //}
         display.textContent=display.textContent+button
     }
 
-    if (button=="+" || button=="-" || button=="*" || button=="/"){
+    if (button=="+" || button=="-" || button=="*" || button=="/" || button=="%"){
         operator=button
         firstNum=display.textContent
+        hasResult=false
         display.textContent=""
     }
 
@@ -38,6 +48,7 @@ function log(e){
         let result=calculate(firstNum,secondNum,operator)
         checkContentLength(result)
         display.textContent=calculate(firstNum,secondNum,operator)
+        hasResult=true
     }
 
 }
@@ -53,7 +64,13 @@ function calculate(firstNumber,secondNumber,operator){
         return Number(firstNumber)*Number(secondNumber)
     }
     if (operator==="/"){
+        if (firstNumber==0 || secondNumber==0){
+            return "Invalid"
+        }
         return Number(firstNumber)/Number(secondNumber)
+    }
+    if (operator==="%"){
+        return Number(firstNumber)%Number(secondNumber)
     }
 }
 
@@ -61,7 +78,21 @@ function calculate(firstNumber,secondNumber,operator){
 
 function checkContentLength(content){
     
-    if (String(content).length>=9){
-        display.style.fontSize="1.3em"
+    if (String(content).length>10 && String(content).length<20){
+        display.style.fontSize="1.5em"
+    }
+    else if (String(content).lengt>=20){
+        display.style.fontSize="1.5em"
+    }
+    else{
+        display.style.fontSize="2em"
     }
 }
+
+//function checkDecimalPointsCount(content){
+//    let regex="/^[^.]*\.[^.]*[^.]$/g"
+//    if (((content.split(".").length)>2 && content.split(".")[1]!=="")){
+//    console.log(content.split("."))
+//    return true
+//}
+//}
